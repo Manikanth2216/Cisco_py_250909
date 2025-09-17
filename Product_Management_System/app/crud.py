4b4c39e0ada33bcdf509167cb6c3993b4ec6c257
meghanakakarla <meghanakakarla666@gmail.com>
2025-09-17 08:58:17 +0530

    .

--- Product_Management_System/app/crud.py
+++ Product_Management_System/app/crud.py
@@ -1,6 +1,8 @@
+"""CRUD operations for Product Management System."""
 from app.models import db, Product
 
 def create_product(product):
+    """Create a new product in the database."""
     product_model = Product(
         id=product['id'],
         name=product['name'],
@@ -10,18 +12,21 @@ def create_product(product):
     db.session.commit()
 
 def read_all_products():
+    """Read all products from the database."""
     products=db.session.query(Product).all()
     dict_products=[]
     for product in products:
         product_dict=product.to_dict()
         dict_products.append(product_dict)
-    return dict_products 
+    return dict_products
 
 def read_model_by_id(id):
+    """Read a product by ID from the database."""
     product=db.session.query(Product).filter_by(id=id).first()
     return product
 
 def read_by_id(id):
+    """Read a product by ID from the database."""
     product=read_model_by_id(id)
     if not product:
         return None
@@ -29,14 +34,18 @@ def read_by_id(id):
     return product_dict
 
 def update(id, new_product):
+    """
+    Update a product in the database.
+    """
     product=read_model_by_id(id)
     if not product:
         return
     product.price=new_product['price']
-    db.session.commit()
-    
-    
+    db.session.commit()   
 def delete_product(id):
+    """
+    Delete a product from the database.
+    """
     product = read_model_by_id(id)
     if not product:
         return
